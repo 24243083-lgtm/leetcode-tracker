b8747c9303ff9b2037ebc2c2cb9e7af2c18ba8b7
Roshan <24243083@drngpit.ac.in>
2026-07-09 10:06:21 +0530

    Create 1724-CustomerWhoVisitedButDidNotMakeAnyTransactions

---
+++ 1724-CustomerWhoVisitedButDidNotMakeAnyTransactions/1724-CustomerWhoVisitedButDidNotMakeAnyTransactions.sql
@@ -0,0 +1,8 @@
+-- Last updated: 7/9/2026, 10:06:32 AM
+# Write your MySQL query statement below
+SELECT v.customer_id, COUNT(*) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
